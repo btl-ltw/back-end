9daa7561f17a2f8551ebea52aba05c7363a414de
MK <qscvdefb@gmail.com>
2024-11-12 16:47:51 +0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,8 +1,19 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *'); // Cho phép tất cả các origin
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    header('Access-Control-Max-Age: 60'); // Cấu hình cache cho preflight request
+    http_response_code(200); // Trả về HTTP OK
+    exit(); // Dừng tiếp tục xử lý
+}
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Max-Age: 60');
+header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
 require '../vendor/autoload.php';
