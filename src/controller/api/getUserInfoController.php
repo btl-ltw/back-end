72becfd9ca0c0fdb9bce86e20538ec7ec478fa94
MK <qscvdefb@gmail.com>
2024-11-12 16:01:40 +0700

    allow cors

--- src/controller/api/getUserInfoController.php
+++ src/controller/api/getUserInfoController.php
@@ -4,8 +4,6 @@ class getUserInfoController extends apiController {
     public function GET () {
         $data = $this->userInfoRepository->getUserInfo($_SESSION['username']);
 
-        $data['role'] = $data['role'] ?? "guest";
-
         $this->customResponseData($data);
     }
 }
